56f00fcab10f4068b7a195652ac750b885770263
Alina Ray <a.alina.ray@gmail.com>
2019-07-18 20:57:21 +0300

    connect was added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+// function createStore(reducer, initialState = {}) {
+//     let callbacks = [];
+//     let state = {...initialState};
+//
+//     return {
+//         getState() {
+//             //read state
+//             return state;
+//         },
+//         subscribe(callback) {
+//             //sabscribing for changing state
+//             //saving funcs to the arr
+//             callbacks.push(callback);
+//         },
+//         dispatch(action) {
+//             //setState
+//             state = reducer(state , action);
+//             // start up of functions from arr
+//             callbacks.forEach(f=>f());
+//         },
+//     }
+// }
+
+// store.subscribe(()=> {
+//     // getting state
+//     // it's like addEventListener
+//     // subscription event
+//     console.log(store.getState());
+// });
 
 class App extends React.Component {
-    state = {
-        count: 0,
-    };
-    increase = () => {
-       this.setState(prevState => ({
-           count: prevState.count + 1
-       }));
-    };
-    decrease = () => {
-        this.setState(prevState => ({
-            count: prevState.count - 1
-        }))
-    };
     render() {
+        // getting count from state
+        const {count, increase, decrease} = this.props;
         return (
             <div>
-                <h1>Count: {this.state.count}</h1>
-                <button onClick={this.increase}>+</button>
-                <button onClick={this.decrease}>-</button>
+                <h1>Count: {count}</h1>
+                <button onClick={increase}>+</button>
+                <button onClick={decrease}>-</button>
             </div>
         )
     };
 }
-export default App;
+
+const mapStateToProp = (state) => {
+    // return objects which will be added to the props of the component
+    return {
+        count: state.count
+    };
+};
+
+const mapDispatchToProp = (dispatch) => {
+    // binding callbacks to the component's props
+    return {
+        increase: () => dispatch({type: 'increase', value: 1}),
+        decrease: () => dispatch({type: 'decrease', value: 1})
+    };
+};
+const AppWithRedux = connect(mapStateToProp, mapDispatchToProp)(App);
+export default AppWithRedux;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const initialState = {count: 0};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increase':
+            return {
+                ...state,
+                count: state.count + action.value
+            };
+        case 'decrease':
+            return {
+                ...state,
+                count: state.count - action.value
+            };
+        default:
+            return state;
+    }
+};
+const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    ,
+    document.getElementById('root')
+);
 
